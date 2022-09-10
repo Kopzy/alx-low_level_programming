60ffc7fd0604fb3eb1c40a1d2f242d24823d47bb
KopnoAbasi <willisonelisha@gmail.com>
2022-09-09 19:44:12 -0700

    hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include	<stdio>
+/**
+ * main - This is a description
+ *
+ * Return: 0 if sucessful,1 if otherwise
+ */
+int	main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return	(0);
+
+}
