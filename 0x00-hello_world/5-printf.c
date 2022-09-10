7e4e355ca7536ba4315810cabe9ac2bab5775469
KopnoAbasi <willisonelisha@gmail.com>
2022-09-09 20:12:31 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include	<stdio.h>
+/**
+ * main - This is a description
+ * Return: 0 if successful, 1 if otherwise
+ */
+int	main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return	(0);
+}
