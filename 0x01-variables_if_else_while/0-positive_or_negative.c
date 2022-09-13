e5e6170bd611857e5d8f416fdbe0581cd93f8d00
KopnoAbasi <willisonelisha@gmail.com>
2022-09-12 19:27:34 -0700

    0-positive_or_negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+#include <stdio.h>
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - A program to check whether or not a variable is positive or otherwise
+ *
+ * Return: 0 if successful
+ *
+ */
+
+int main(void)
+
+{
+
+		int n;
+
+
+
+		srand(time(0));
+
+		n = rand() - RAND_MAX / 2;
+		if	(n > 0)
+		{
+			printf("%d is positive\n", n);
+		}
+		if	(n == 0)
+		{
+			printf("%d is zero\n", n);
+		}
+		if	(n < 0)
+		{
+			printf("%d is a positive number\n", n);
+		}
+
+		return (0);
+
+}
