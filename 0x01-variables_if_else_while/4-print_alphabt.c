1a2512697f7e8a10e4eec54e305b0d62ef305143
KopnoAbasi <willisonelisha@gmail.com>
2022-09-12 19:21:22 -0700

    aphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+		if(a ==
+	}
+}
