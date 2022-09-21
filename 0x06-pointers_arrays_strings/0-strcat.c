906d278055fd98c4d0ea5f3e126489e547581bbd
Kopzy <willisonelisha@gmail.com>
2022-09-21 14:45:40 -0700

    concate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,21 +2,16 @@
 /**
  * *_strcat - A function to concate two strings
  *
- * dest: string destination for concatenates
+ * @dest: string destination for concatenates
  *
- * src: string source code to be concatenated
+ * @src: string source code to be concatenated
  *
  * Return: dest
  *
  */
 char *_strcat(char *dest, char *src)
 {
-	char dest;
-	char src;
+	strcat(*dest, *src);
+	_putchar(*dest);
 
-	dest[100] = "Hello ";
-	src[] = "World!";
-	_strcat(dest, src);
-	_putchar(dest);
-
-	return (dest);
+	return (*dest);
